c24c9fbd26b2df985222743a06e0fb0a2802a9c7
Claudio André Ferreria Caron <140202178+Claudio-Caron@users.noreply.github.com>
2025-02-21 12:48:33 -0400

    Projeto Inicial | config repository

---
+++ UsuariosAPI/Data/UsuarioContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UsuariosAPI.Models;
+
+namespace UsuariosAPI.Data
+{
+    public class UsuarioContext:IdentityDbContext<Usuario>
+    {
+        public UsuarioContext(DbContextOptions<UsuarioContext> opts):base(opts)
+        {
+            
+        }
+
+    }
+}
